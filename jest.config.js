4680244592a3fdc25bd21be618c7199829af086a
Steven Levine <slevine111@gmail.com>
2019-10-18 01:20:22 -0400

    real tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests-fake/**/*.spec.ts'],
+  testMatch: ['**/tests/**/*.spec.ts'],
   setupFiles: ['dotenv/config'],
   globalSetup: './jest-global-setup.ts',
   globalTeardown: './jest-global-teardown.ts'
